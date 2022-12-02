30c90d50200b81da87dbcf249632fa1bc1bea087
Eddie <crazy72eddie@gmail.com>
2022-12-02 13:09:54 +0100

    Finish exercises up to 07

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,5 @@
 const reverseString = function(string) {
-    let letterArray = string.split("");
-    let reversedArray = letterArray.reverse();
-    let resultString = reversedArray.join("");
-    return resultString;
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return Number.isInteger(year /4) && (!(Number.isInteger(year / 100)) || (Number.isInteger(year / 400)));
 };
 
 // Do not edit below this line
